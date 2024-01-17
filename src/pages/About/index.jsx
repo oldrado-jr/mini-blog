9148696af801dce3b1f2ab8a007782a69198927c
Oldrado Junior <oldradojunior@gmail.com>
2024-01-16 21:58:37 -0300

    feat: add about page content and style

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,7 +1,17 @@
+import { Link } from "react-router-dom";
+
+import styles from './styles.module.css';
+
 function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        Sobre o Mini <span>Blog</span>
+      </h2>
+      <p>
+        Este projeto consiste em um blog feito com React no frontend e Firebase no backend.
+      </p>
+      <Link to="/posts/create" className="btn">Criar post</Link>
     </div>
   );
 }
