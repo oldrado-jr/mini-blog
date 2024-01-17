a4ec2bf72e51d55d0a0c4939d3a0373d339bc5aa
Oldrado Junior <oldradojunior@gmail.com>
2024-01-17 17:52:44 -0300

    feat: add create post page content and style

--- src/pages/CreatePost/index.jsx
+++ src/pages/CreatePost/index.jsx
@@ -1,7 +1,70 @@
+import { useState } from "react";
+
+import styles from './styles.module.css';
+
 function CreatePost() {
+  const [title, setTitle] = useState('');
+  const [image, setImage] = useState('');
+  const [body, setBody] = useState('');
+  const [tags, setTags] = useState([]);
+  const [formError, setFormError] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>CreatePost</h1>
+    <div className={styles['create-post']}>
+      <h2>Criar post</h2>
+      <p>Escreva sobre o que quiser e compartilhe o seu conhecimento!</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Título:</span>
+          <input
+            type="text"
+            name="title"
+            required
+            placeholder="Pense num bom título..."
+            value={title}
+            onChange={(e) => setTitle(e.target.value.trim())}
+          />
+        </label>
+        <label>
+          <span>URL da imagem:</span>
+          <input
+            type="url"
+            name="image"
+            required
+            placeholder="Insira uma URL da imagem que representa o seu post"
+            value={image}
+            onChange={(e) => setImage(e.target.value.trim())}
+          />
+        </label>
+        <label>
+          <span>Conteúdo:</span>
+          <textarea
+            name="body"
+            required
+            placeholder="Insira o conteúdo do post"
+            value={body}
+            onChange={(e) => setBody(e.target.value.trim())}
+          ></textarea>
+        </label>
+        <label>
+          <span>Tags:</span>
+          <input
+            type="text"
+            name="tags"
+            required
+            placeholder="Insira as tags separadas por vírgula"
+            value={tags}
+            onChange={(e) => setTags(e.target.value.trim())}
+          />
+        </label>
+        {/* {!loading && <button className="btn">Cadastrar</button>}
+        {loading && <button className="btn" disabled>Aguarde...</button>} */}
+        {formError && <p className="error">{formError}</p>}
+      </form>
     </div>
   );
 }
