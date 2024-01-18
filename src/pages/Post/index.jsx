d3e8276222b06e458cc7b6833fbf72efcfd40dcc
Oldrado Junior <oldradojunior@gmail.com>
2024-01-18 20:45:30 -0300

    feat: add post page

---
+++ src/pages/Post/index.jsx
@@ -0,0 +1,13 @@
+import { useParams } from 'react-router-dom';
+
+function Post() {
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h1>Post {id}</h1>
+    </div>
+  );
+}
+
+export default Post;
