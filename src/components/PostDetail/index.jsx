c8cb3bc4b012af7420bfebb4fdeeb1dd27dc2c9e
Oldrado Junior <oldradojunior@gmail.com>
2024-01-18 19:24:17 -0300

    feat: add post detail style

--- src/components/PostDetail/index.jsx
+++ src/components/PostDetail/index.jsx
@@ -1,15 +1,17 @@
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import styles from './styles.module.css';
+
 function PostDetail({ post }) {
   const { uid, title, image, tagsList, createdBy } = post;
 
   return (
-    <div>
+    <div className={styles['post-detail']}>
       <img src={image} alt={title} />
       <h2>{title}</h2>
-      <p>{createdBy}</p>
-      <div>
+      <p className={styles['created-by']}>{createdBy}</p>
+      <div className={styles.tags}>
         {tagsList.map((tag) => (
           <p key={tag}>
             <span>#</span>
